b2293e24b9e6ff4cb9318880f6237275c5ef1910
goreleaserbot <goreleaser@carlosbecker.com>
2021-03-12 16:53:11 +0200

    Brew formula update for bubbly version 0.0.1-alpha

--- Formula/bubbly.rb
+++ Formula/bubbly.rb
@@ -10,22 +10,20 @@ class Bubbly < Formula
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/valocode/bubbly/releases/download/0.0.1-alpha/bubbly_0.0.1-alpha_darwin_amd64.tar.gz"
-    sha256 "140cdace9ab5f2ae97378970ff58ce5c0d63d14a06ca9a2cf1e4d62ef8d055b8"
+    sha256 "80b34e766eca13f9ef765f85444a077e36996f9db93641c195e5d17e6ec61318"
   end
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/valocode/bubbly/releases/download/0.0.1-alpha/bubbly_0.0.1-alpha_darwin_arm64.tar.gz"
-    sha256 "3dc54e32c9b0b89d9bbe0a5504a393f11a3844248d04193e63afb68c0639e53b"
+    sha256 "26a840896b61eab2ba7fe2b1efc237955449ef8176529687654b84d011af4c4f"
   end
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/valocode/bubbly/releases/download/0.0.1-alpha/bubbly_0.0.1-alpha_linux_amd64.tar.gz"
-    sha256 "254fe0b2cb38579d83b6f57e8aec35f14e8dcbfa096901d7c34a500e700eeab9"
+    sha256 "2f4ff89754e4738a0fa162d2ad276740d28205e4c73e7a921196d7dc31675ca9"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
     url "https://github.com/valocode/bubbly/releases/download/0.0.1-alpha/bubbly_0.0.1-alpha_linux_arm64.tar.gz"
-    sha256 "0e2ffb1c00437bc2cab5043194e446a2f0b54f80055322383e85f37a6483a5e8"
+    sha256 "8f342b70b8859771b7b647afcd825fe0f7ecfae1435271a4f2e8fd116d30a1d5"
   end
-
-  depends_on "go"
 
   def install
     bin.install "bubbly"
